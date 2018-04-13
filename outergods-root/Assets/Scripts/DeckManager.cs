6cf0c822ecdd0ae2a8a7e0b425bc656c1b14b9c6
Erick Eiben <eeiben1@student.fitchburgstate.edu>
2018-04-13 15:27:28 -0400

    Set up framework for the project, began working on the CardCreator and DeckManager scripts

---
+++ outergods-root/Assets/Scripts/DeckManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardManager
+{
+    public class DeckManager : MonoBehaviour
+    {
+        public GameObject cardTemplate;
+        public List<int> cardID = new List<int>();
+
+        private void CreateCard(int cardID)
+        {
+
+        }
+    }
+}
